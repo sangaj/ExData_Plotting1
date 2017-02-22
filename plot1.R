88ed408c70a3f540dad5d617e90093cc07f8764d
sangaj <gubingjing@hotmail.com>
2017-02-22 02:15:10 +0100

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,16 @@
+if(!file.exists("exdata-data-household_power_consumption.zip")) {
+  temp <- tempfile()
+  download.file("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+  file <- unzip(temp)
+  unlink(temp)
+}
+
+
+data <- read.table(file, header=T, sep=";",na.strings = "?")
+subdata <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+subdata$Date <- strptime(subdata$Date,"%d/%m/%Y")
+subdata$Global_active_power <- as.numeric(as.character(subdata$Global_active_power))
+hist(subdata$Global_active_power, main = paste("Global Active Power"), col="red", xlab="Global Active Power (kilowatts)")
+dev.copy(png, file="plot1.png", width=480, height=480)
+dev.off()
+
---
+++ plot3.R
@@ -0,0 +1,20 @@
+if(!file.exists("exdata-data-household_power_consumption.zip")) {
+  temp <- tempfile()
+  download.file("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+  file <- unzip(temp)
+  unlink(temp)
+}
+
+Sys.setlocale(category = "LC_ALL", locale = "english")
+data <- read.table(file, header=T, sep=";",na.strings = "?")
+subdata <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+datetime <- strptime(paste(subdata$Date, subdata$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+subMetering1 <- as.numeric(subdata$Sub_metering_1)
+subMetering2 <- as.numeric(subdata$Sub_metering_2)
+subMetering3 <- as.numeric(subdata$Sub_metering_3)
+plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=1, pt.cex = 1,col=c("black", "red", "blue"),cex=0.6)
+dev.copy(png, file="plot3.png", width=480, height=480)
+dev.off()
