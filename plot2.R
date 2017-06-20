29458305adf3ee5a7834b07f725d3a631a41b5f2
sangaj <gubingjing@hotmail.com>
2017-06-20 10:01:04 +0200

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,3 +1,4 @@
+### Check the data file otherwise download and unzip it
 if(!file.exists("exdata-data-household_power_consumption.zip")) {
   temp <- tempfile()
   download.file("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
@@ -5,7 +6,8 @@ if(!file.exists("exdata-data-household_power_consumption.zip")) {
   unlink(temp)
 }
 
-Sys.setlocale(category = "LC_ALL", locale = "english")
+### read data and plot it 
+Sys.setlocale(category = "LC_ALL", locale = "english") # change the local language for plot
 data <- read.table(file, header=T, sep=";",na.strings = "?")
 subdata <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 datetime <- strptime(paste(subdata$Date, subdata$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
